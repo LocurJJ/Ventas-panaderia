4414e8c350babcda503384a6f166a31feae81930
LocurJJ <127799708+LocurJJ@users.noreply.github.com>
2026-06-18 15:36:18 -0300

    Forzar tema noche actualizado en ventas

--- firebase.js
+++ firebase.js
@@ -1,2 +1,3 @@
 document.write('<script src="https://cdn.jsdelivr.net/gh/LocurJJ/Ventas-panaderia@5de2a55b35c9b698bf4c06d49d682963ab2123f3/firebase.js"><\/script>');
+document.write('<link rel="stylesheet" href="night-sales.css?v=20260618-2">');
 document.write('<script src="enter-pay.js?v=20260618-1"><\/script>');
